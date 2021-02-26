5ca8bdfaadd4d4ba964130759479bc31480f9d95
Lario <osunkunleolanrewaju@gmail.com>
2021-02-26 20:17:38 +0100

    button error fixed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,32 +1,31 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import './Navbar.css'
 import {FaLaptopCode} from 'react-icons/fa'
 import {FaBars, FaTimes} from 'react-icons/fa'
-import { Button } from '../Button/Button'
 import { IconContext } from 'react-icons/lib'
 
 
 function Navbar() {
     const [click, setClick] = useState(false)
-    const [button, setButton] = useState(true)
+    // const [button, setButton] = useState(true)
 
     const handleClick = () => setClick(!click)
     const closeMobileMenu = () => setClick(false)
 
-    const showButton = () =>{
-        if (window.innerWidth <= 960){
-            setButton(false)
-        }else{
-            setButton(true)
-        }
-    }
+//  const showButton = () =>{
+//         if (window.innerWidth <= 960){
+//             setButton(false)
+//         }else{
+//             setButton(true)
+//         }
+//     }   
 
-    useEffect(()=>{
-        showButton()
-    }, [])
+    // useEffect(()=>{
+    //     showButton()
+    // }, [])
 
-    window.addEventListener('resize', showButton)
+    // window.addEventListener('resize', showButton)
 
     return (
         <>
@@ -56,21 +55,6 @@ function Navbar() {
                                 Products
                             </Link>
                         </li>
-                        {/* <li className='nav-btn'>
-                            {button ? (
-                                <Link to='/sign-up' className='btn-link'>
-                                    <Button buttonStyle='btn--outline' onClick={closeMobileMenu} >
-                                        SIGN UP 
-                                    </Button>
-                                </Link>
-                            ) : (
-                                <Link to='/sign-up' className='btn-link'>
-                                    <Button buttonStyle='btn--outline' buttonSize='btn--mobile' onClick={closeMobileMenu}>
-                                        SIGN UP 
-                                    </Button>
-                                </Link>
-                            )}
-                        </li> */}
                     </ul>
                 </div>
             </div>
