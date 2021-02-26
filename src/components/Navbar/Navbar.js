88e9f9466c668624472a147ed8eb1b8f7d22f1f8
Lario <osunkunleolanrewaju@gmail.com>
2021-02-26 14:10:27 +0100

    rewite the navbar components

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,45 +1,82 @@
-import React, { Component } from 'react'
-import {FaAlignRight} from 'react-icons/fa'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import './Navbar.css'
+import {FaLaptopCode} from 'react-icons/fa'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import { Button } from '../Button/Button'
+import { IconContext } from 'react-icons/lib'
 
 
-export class Navbar extends Component {
-    state={
-        isOpen : false
+function Navbar() {
+    const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true)
+
+    const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false)
+
+    const showButton = () =>{
+        if (window.innerWidth <= 960){
+            setButton(false)
+        }else{
+            setButton(true)
+        }
     }
-    handleToggle = () => {
-        this.setState({isOpen: !this.state.isOpen})
-    }
-    render() {
-        const {isOpen} = this.state
-        return (
-            <nav className="navbar">
-                <div className="nav-center">
-                    <div className="nav-header">
-                        <Link to="/" className="nav-logo">
-                            Lario
-                        </Link>
-                        <button
-                            type="button"
-                            className="nav-btn"
-                            onClick={this.handleToggle} 
-                        >
-                            <FaAlignRight className="nav-icon" />
-                        </button>
+
+    useEffect(()=>{
+        showButton()
+    }, [])
+
+    window.addEventListener('resize', showButton)
+
+    return (
+        <>
+        <IconContext.Provider value={{color: '#fff'}}>
+            <div className='navbar'>
+                <div className='navbar-container container'>
+                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                        <FaLaptopCode className='navbar-icon' />
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        {click ? <FaTimes /> : <FaBars />}
                     </div>
-                    <ul className={isOpen?"nav-links show-nav" : "nav-links"}>
-                        <li>
-                            <Link to="/">Home</Link>
+
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
                         </li>
-                        <li>
-                            <Link to="/rooms">Rooms</Link>
+                        <li className='nav-item'>
+                            <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                                Services
+                            </Link>
                         </li>
+                        <li className='nav-item'>
+                            <Link to='/products' className='nav-links' onClick={closeMobileMenu} >
+                                Products
+                            </Link>
+                        </li>
+                        {/* <li className='nav-btn'>
+                            {button ? (
+                                <Link to='/sign-up' className='btn-link'>
+                                    <Button buttonStyle='btn--outline' onClick={closeMobileMenu} >
+                                        SIGN UP 
+                                    </Button>
+                                </Link>
+                            ) : (
+                                <Link to='/sign-up' className='btn-link'>
+                                    <Button buttonStyle='btn--outline' buttonSize='btn--mobile' onClick={closeMobileMenu}>
+                                        SIGN UP 
+                                    </Button>
+                                </Link>
+                            )}
+                        </li> */}
                     </ul>
-                </div>   
-            </nav>
-        )
-    }
+                </div>
+            </div>
+        </IconContext.Provider>
+        </>
+    )
 }
 
 export default Navbar
