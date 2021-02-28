4b81ee32dec01efe139fec703ffe88c5d7efbfd4
Lario <osunkunleolanrewaju@gmail.com>
2021-02-28 19:16:09 +0100

    added skillset to the about me page

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,6 +3,9 @@ import { AboutMeData } from '../../data/AboutMeData'
 import AboutMeImg from '../../assets/images/lario-2.jpg'
 import './AboutMe.css'
 import Title from '../Title/Title'
+import {FaLaptopCode} from 'react-icons/fa'
+import {BsPencilSquare} from 'react-icons/bs'
+import {BsBag} from 'react-icons/bs'
 
 const AboutMe = () => {
     return (
@@ -24,19 +27,31 @@ const AboutMe = () => {
         <Title title="What I Do" />
         <div className="skillset">
             <div>
-                <div className="icon"></div>
+                <div className="icon">
+                    <FaLaptopCode /> 
+                </div>
                 <h3>Frontend Developement</h3>
-                <p></p>
+                <p>
+                I build awesome client-side applications with the modern Technologies.
+                </p>
             </div>
             <div>
-                <div className="icon"></div>
-                <h3>Frontend Developement</h3>
-                <p></p>
+                <div className="icon">
+                    <BsBag />
+                </div>
+                <h3>Skill-set</h3>
+                <p>
+                React.js & Redux, SASS, Git & Github, Firebase, CSS3, HTML 5 and NodeJs
+                </p>
             </div>
             <div>
-                <div className="icon"></div>
-                <h3>Frontend Developement</h3>
-                <p></p>
+                <div className="icon">
+                    <BsPencilSquare />
+                </div>
+                <h3>Technical Writing</h3>
+                <p>
+                I write technical articles to share my knowledge with other developers and also for the tech community.
+                </p>
             </div>
         </div>
         </div>
