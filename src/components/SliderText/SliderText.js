f5c86496db7bdbb53dbe06f1484a1418536df70a
Lario <osunkunleolanrewaju@gmail.com>
2021-03-02 08:50:53 +0100

    added transition and text slider to the project

--- src/components/SliderText/SliderText.js
+++ src/components/SliderText/SliderText.js
@@ -15,7 +15,7 @@ const SliderText = ({text}) => {
   React.useEffect(() => {
     const intervalId = setInterval(() =>
       setIndex(index => index + 1),
-      3000 // every 3 seconds
+      5000 // every 3 seconds
     );
     return () => clearTimeout(intervalId);
   }, []);
